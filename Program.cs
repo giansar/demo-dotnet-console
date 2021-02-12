87e3b588c94f17e425a05709373837ab3f6f6fe1
Giansar Pratiknya <gpratiknya@gmail.com>
2021-02-12 09:32:57 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace demo_dotnet_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("The current time is " + DateTime.Now);
+        }
+    }
+}
